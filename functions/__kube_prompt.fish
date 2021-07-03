bbb2c4bc511970b60df51bfbfb2289f6161b489b
Maxim Ivanov <ulidtko@gmail.com>
2021-07-03 17:48:46 +0300

    Crossplatform fix for the stat_mtime issue (#11)
    * fix functions path
    
    * use test
    
    * fix #5
    
    * Add cross-platform stat_mtime via python
    
    Fixes #1 properly. Fixes #5 too. Makes both MacOS and Linux users happy.
    
    Co-authored-by: jkatagi <kemisknkn37@gmail.com>

--- functions/__kube_prompt.fish
+++ functions/__kube_prompt.fish
@@ -5,7 +5,7 @@
 function __kube_ps_update_cache
   function __kube_ps_cache_context
     set -l ctx (kubectl config current-context 2>/dev/null)
-    if /bin/test $status -eq 0
+    if test $status -eq 0
       set -g __kube_ps_context "$ctx"
     else
       set -g __kube_ps_context "n/a"
@@ -14,19 +14,26 @@ function __kube_ps_update_cache
 
   function __kube_ps_cache_namespace
     set -l ns (kubectl config view --minify -o 'jsonpath={..namespace}' 2>/dev/null)
-    if /bin/test -n "$ns"
+    if test -n "$ns"
       set -g __kube_ps_namespace "$ns"
     else
       set -g __kube_ps_namespace "default"
     end
   end
 
+  function __stat_mtime
+    #-- cross-platform workaround; POSIX didn't specify stat(1) and so
+    #-- its interface is incompatibly different on Mac OS and Linux.
+    #-- see https://unix.stackexchange.com/q/561927/3097
+    python -c "print(__import__('os').stat(__import__('sys').argv[1]).st_mtime)" $argv
+  end
+
   set -l kubeconfig "$KUBECONFIG"
-  if /bin/test -z "$kubeconfig"
+  if test -z "$kubeconfig"
     set kubeconfig "$HOME/.kube/config"
   end
 
-  if /bin/test "$kubeconfig" != "$__kube_ps_kubeconfig"
+  if test "$kubeconfig" != "$__kube_ps_kubeconfig"
     __kube_ps_cache_context
     __kube_ps_cache_namespace
     set -g __kube_ps_kubeconfig "$kubeconfig"
@@ -35,8 +42,8 @@ function __kube_ps_update_cache
   end
 
   for conf in (string split ':' "$kubeconfig")
-    if /bin/test -r "$conf"
-      if /bin/test -z "$__kube_ps_timestamp"; or /bin/test (/usr/bin/stat -f '%m' "$conf") -gt "$__kube_ps_timestamp"
+    if test -r "$conf"
+      if test -z "$__kube_ps_timestamp"; or test (__stat_mtime "$conf") -gt "$__kube_ps_timestamp"
         __kube_ps_cache_context
         __kube_ps_cache_namespace
         set -g __kube_ps_kubeconfig "$kubeconfig"
@@ -48,7 +55,7 @@ function __kube_ps_update_cache
 end
 
 function __kube_prompt
-  if /bin/test -z "$__kube_ps_enabled"; or /bin/test $__kube_ps_enabled -ne 1
+  if test -z "$__kube_ps_enabled"; or test $__kube_ps_enabled -ne 1
     return
   end
 
